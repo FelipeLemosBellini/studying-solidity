dd69dbaa211cb9e6fc9ff7bcf25e09178da00661
FelipeLemosBellini <felipelemosbellini@gmail.com>
2025-02-11 17:52:14 -0300

    create interfaces

--- .deps/studying-solidity/InheritanceContract.sol
+++ .deps/studying-solidity/InheritanceContract.sol
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract InheritanceContract {
+interface IInheritanceContract {
+    function createInheritance(
+        address[] memory _addresses,
+        uint8[] memory _percentagens
+    ) external payable;
+
+    function getMyTestament()
+        external
+        view
+        returns (
+            address[] memory,
+            uint8[] memory,
+            uint256,
+            uint256
+        );
+
+    function updateProofOfLife() external;
+}
+
+contract InheritanceContract is IInheritanceContract {
     struct Inheritor {
         address inheritorAddress;
         uint8 percentage;
@@ -11,15 +30,18 @@ contract InheritanceContract {
         Inheritor[] inheritors;
         bool exist;
         uint256 lastProofOfLife;
+        uint256 amount;
         //salvar as moedas aqui
     }
 
     mapping(address => Testator) public testators;
 
+    mapping(address => address) public inheritorToTestator;
+
     function createInheritance(
         address[] memory _addresses,
         uint8[] memory _percentagens
-    ) external {
+    ) external payable {
         address own = msg.sender;
 
         if (testators[own].exist) delete testators[own].inheritors;
@@ -30,6 +52,7 @@ contract InheritanceContract {
                 _percentagens[i]
             );
 
+            inheritorToTestator[_addresses[i]] = own;
             testators[own].inheritors.push(_inheritor);
         }
         testators[own].exist = true;
@@ -39,27 +62,38 @@ contract InheritanceContract {
     function getMyTestament()
         external
         view
-        returns (address[] memory, uint8[] memory)
+        returns (
+            address[] memory,
+            uint8[] memory,
+            uint256, //timestamp last proof of life
+            uint256 //amount
+        )
     {
         address own = msg.sender;
-        require(testators[own].exist, "Testador nao encontrado");
+
+        Testator memory _testator = testators[own];
+
+        require(_testator.exist, "Testador nao encontrado");
 
         address[] memory _inheritorAddresses = new address[](
-            testators[own].inheritors.length
+            _testator.inheritors.length
         );
 
-        uint8[] memory _percentages = new uint8[](
-            testators[own].inheritors.length
-        );
+        uint8[] memory _percentages = new uint8[](_testator.inheritors.length);
 
-        for (uint256 i = 0; i < testators[own].inheritors.length; i++) {
-            Inheritor memory _currentInheritor = testators[own].inheritors[i];
+        for (uint256 i = 0; i < _testator.inheritors.length; i++) {
+            Inheritor memory _currentInheritor = _testator.inheritors[i];
 
             _inheritorAddresses[i] = (_currentInheritor.inheritorAddress);
             _percentages[i] = (_currentInheritor.percentage);
         }
 
-        return (_inheritorAddresses, _percentages);
+        return (
+            _inheritorAddresses,
+            _percentages,
+            _testator.lastProofOfLife,
+            _testator.amount
+        );
     }
 
     function checkIfTestatorExist() private view returns (bool) {
@@ -77,6 +111,8 @@ contract InheritanceContract {
         require(testators[own].exist, "Testador nao existe");
         testators[own].lastProofOfLife = block.timestamp;
     }
+
+    function checkInheritance() external {}
 }
 
 /*{
